a620ffe8df0a411c0d321e7a639e5d334e39ad64
roger87 <willy32136@gmail.com>
2020-07-05 14:21:28 +0800

    新增函式

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 console.log("我是 JS!!!");
 
-document.getElementById("box1").innerHTML = ("哈囉~");
+document.getElementById("box1").innerHTML = ("X__X");
 
-var a = 10;
+var a = 6;
 var b = 1.5;
 var c = "文字";
 var d = true;
 
 console.log(a);
 
-a = b + 1;
+a = c * 95;
 
 console.log(a);
+
+//指定符號**
+var a = 66;
+
+a = a**3;
+
+console.log(a);
+
+var num1 = 66;
+
+ num1 += 6;
+
+ console.log("指定運算子" + num1);
+
+function functionA() {
+    console.log("我是函式X__X")
+}
+
+functionA();
