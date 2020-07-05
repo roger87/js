71cd7153f81513dbafa50453def8ada3a9eb7aca
roger87 <willy32136@gmail.com>
2020-07-05 12:41:18 +0800

    增加運算子

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 console.log("我是 JS!!!");
 
 document.getElementById("box1").innerHTML = ("哈囉~");
+
+var a = 10;
+var b = 1.5;
+var c = "文字";
+var d = true;
+
+console.log(a);
+
+a = b + 1;
+
+console.log(a);
